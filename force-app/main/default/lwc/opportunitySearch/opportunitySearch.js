1ff48901a594868e2be9069f0a8b6d2f466d46cd
Raphael Houdry <raphael-59200@hotmail.fr>
2023-01-23 17:55:14 +0100

    finished

--- force-app/main/default/lwc/opportunitySearch/opportunitySearch.js
+++ force-app/main/default/lwc/opportunitySearch/opportunitySearch.js
@@ -1,29 +1,38 @@
-import { LightningElement, track, api, wire } from 'lwc';
+import { LightningElement, track, api } from 'lwc';
 import getOppt from '@salesforce/apex/OpportunitySearchFromAccount.getOppt';
 import { NavigationMixin } from 'lightning/navigation';
 
 export default class OpportunitySearch extends NavigationMixin(LightningElement) {
-    @api recordId;
 
-    // repasser cet api en @wire
-    @api foundOppt;
+    @api recordId;
+    @track foundOppt;
 
     textValue = " ";
 
+    // Method to get user input
     handleSearchText(event) {
         this.textValue = event.detail.value;
     }
 
+    handleEnter(event) {
+        if(event.keyCode === 13) {
+            this.searchOpptFromAccount();
+        }
+    }
+
+    // Method called when use click on Search Button
     searchOpptFromAccount() {
+        // retrieve data (opportunities related to the account) according to user's input
         getOppt({searchText: this.textValue, accountID: this.recordId})
             .then(result => {
                 this.foundOppt = this.addSlashToId(result);
             })
             .catch(error => {
-                this.foundOppt = null;
+                this.foundOppt = [];
             });
     }
 
+    // Method to add a slash
     addSlashToId(foundOpptWithoutSlashToId) {
         foundOpptWithoutSlashToId.forEach(opp => {
             opp.linkName = '/' + opp.Id;
@@ -31,6 +40,8 @@ export default class OpportunitySearch extends NavigationMixin(LightningElement)
         });
         return foundOpptWithoutSlashToId;
     }
+
+    // columns used in the lightning datatable
     cols = [
         {
             label:'Name', 
@@ -40,10 +51,22 @@ export default class OpportunitySearch extends NavigationMixin(LightningElement)
                 label: {fieldName: 'Name'},
                 target: '_blank'
             }
-        } ,
-        {label:'Stage', fieldName:'StageName' , type:'picklist'} ,
-        {label:'Amount', fieldName:'Amount' , type:'currency'},
-        {label:'Close Date', fieldName:'CloseDate' , type:'date'}    
+        },
+        {
+            label:'Stage', 
+            fieldName:'StageName' , 
+            type:'picklist'
+        },
+        {
+            label:'Amount', 
+            fieldName:'Amount' , 
+            type:'currency'
+        },
+        {
+            label:'Close Date',
+            fieldName:'CloseDate' , 
+            type:'date'
+        }    
     ]
 
 }
